909f9c821201e019c54f72212a820fe06d6aaf10
allx-under <95172339+allx-under@users.noreply.github.com>
2022-05-22 23:05:50 +0300

    add task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery")
+
+const galleryItemsEl = galleryItems.map((item) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}" rel="noopener noreferrer" target="_blank">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+}).join("");
+
+galleryContainer.innerHTML = galleryItemsEl;
+
+// galleryContainer.addEventListener('click', e => {
+//     if (e.target.dataset.source) {
+//       const instance = basicLightbox.create(`
+//     <img src="${e.target.href}" width="800" height="600">
+// `)
+//         instance.show()
+//     }
+// })
+
+const imageEl = document.querySelector(".gallery__link")
+
+imageEl.addEventListener("click", (e) => console.log(e.target))
