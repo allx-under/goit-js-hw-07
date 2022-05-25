47896eaa9ce7f1c25ad8e98c3e625f6a51ea6321
allx-under <95172339+allx-under@users.noreply.github.com>
2022-05-25 19:24:15 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery")
+
+const galleryItemsEl = galleryItems.map((item) => {
+    return `<li><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></li>`
+}).join("");
+
+galleryContainer.innerHTML = galleryItemsEl;
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ })
